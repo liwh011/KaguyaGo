486138dd28cd0c61d2e4b47eb2eb1f6740c4cfec
liwh011 <304180208@qq.com>
2022-05-30 15:36:43 +0800

    完成骰子

--- plugin/dice/view.go
+++ plugin/dice/view.go
@@ -18,10 +18,33 @@ type DicePlugin struct{}
 func (DicePlugin) GetPluginInfo() gonebot.PluginInfo {
 	return gonebot.PluginInfo{
 		Name:        "dice",
-		Description: "dice",
+		Description: "跑团骰子",
 		Version:     "0.0.1",
 		Author:      "liwh011",
 	}
+}
+
+func (DicePlugin) Init(engine *gonebot.EngineProxy) {
+	checkMw := func(ctx *gonebot.Context) bool {
+		params, err := parseText(ctx.Event.ExtractPlainText())
+		if err != nil {
+			ctx.Reply(err.Error())
+			ctx.Abort()
+			return false
+		}
+		ctx.Set("dice", params)
+		return true
+	}
+
+	engine.
+		NewHandler(gonebot.EventNameGroupMessage).
+		Use(gonebot.StartsWith(".r")).
+		Use(checkMw).
+		Handle(func(ctx *gonebot.Context) {
+			params := ctx.MustGet("dice").(diceParams)
+			res := doDice(params)
+			ctx.Reply("你的投掷结果是", res)
+		})
 }
 
 type diceParams struct {
@@ -32,62 +55,67 @@ type diceParams struct {
 	offset int // 偏移
 }
 
-func (DicePlugin) Init(engine *gonebot.EngineProxy) {
-	dicePat := regexp.MustCompile(`^\.r\s*((?P<num>\d{0,2})d((?P<min>\d{1,4})~)?(?P<max>\d{0,4})((?P<sign>[+-])(?P<offset>\d{0,5}))?)?\b`)
-	names := dicePat.SubexpNames()
-	checkMw := func(ctx *gonebot.Context) bool {
-		text := ctx.Event.ExtractPlainText()
-		matchGroup := dicePat.FindStringSubmatch(text)
-		if matchGroup == nil {
-			return false
-		}
-
-		matchMap := make(map[string]string)
-		for i, name := range names {
-			if i == 0 {
-				continue
-			}
-			matchMap[name] = matchGroup[i]
-		}
-		params := diceParams{
-			num:    1,
-			min:    1,
-			max:    100,
-			sign:   1,
-			offset: 0,
-		}
-		if matchMap["num"] != "" {
-			params.num, _ = strconv.Atoi(matchMap["num"])
-		}
-		if matchMap["min"] != "" {
-			params.min, _ = strconv.Atoi(matchMap["min"])
-		}
-		if matchMap["max"] != "" {
-			params.max, _ = strconv.Atoi(matchMap["max"])
-		}
-		if matchMap["sign"] != "" {
-			if matchMap["sign"] == "-" {
-				params.sign = -1
-			} else {
-				params.sign = 1
-			}
-		}
-		if matchMap["offset"] != "" {
-			params.offset, _ = strconv.Atoi(matchMap["offset"])
-		}
-
-		ctx.Set("dice", params)
-		return true
+func parseText(text string) (params diceParams, err error) {
+	dicePat := regexp.MustCompile(`^\.r\s*((?P<num>\d*)d((?P<min>\d+)~)?(?P<max>\d*)((?P<sign>[+-])(?P<offset>\d*))?)?$`)
+	matchGroup := dicePat.FindStringSubmatch(text)
+	if matchGroup == nil {
+		err = fmt.Errorf("投骰子的格式为：.r <数量>d[最小值~]<最大值>[+|-偏移量]，例如：.r 3d6+1")
+		return
 	}
 
-	engine.
-		NewHandler(gonebot.EventNameGroupMessage).
-		Use(checkMw).
-		Handle(func(ctx *gonebot.Context) {
-			params := ctx.MustGet("dice").(diceParams)
-			res := doDice(params)
-			ctx.Reply("结果是", res)
-		})
+	matchMap := make(map[string]string)
+	for i, name := range dicePat.SubexpNames() {
+		if i == 0 {
+			continue
+		}
+		matchMap[name] = matchGroup[i]
+	}
+
+	// 默认值
+	params = diceParams{
+		num:    1,
+		min:    1,
+		max:    100,
+		sign:   1,
+		offset: 0,
+	}
+	if matchMap["num"] != "" {
+		if len(matchMap["num"]) > 2 {
+			err = fmt.Errorf("骰子数量不能超过99")
+			return
+		}
+		params.num, _ = strconv.Atoi(matchMap["num"])
+	}
+	if matchMap["min"] != "" {
+		if len(matchMap["min"]) > 4 {
+			err = fmt.Errorf("最小值不能超过9999")
+			return
+		}
+		params.min, _ = strconv.Atoi(matchMap["min"])
+	}
+	if matchMap["max"] != "" {
+		if len(matchMap["max"]) > 4 {
+			err = fmt.Errorf("最大值不能超过9999")
+			return
+		}
+		params.max, _ = strconv.Atoi(matchMap["max"])
+	}
+	if matchMap["sign"] != "" {
+		if matchMap["sign"] == "-" {
+			params.sign = -1
+		} else {
+			params.sign = 1
+		}
+	}
+	if matchMap["offset"] != "" {
+		if len(matchMap["offset"]) > 5 {
+			err = fmt.Errorf("偏移量不能超过99999")
+			return
+		}
+		params.offset, _ = strconv.Atoi(matchMap["offset"])
+	}
+
+	return
 }
 
 type diceResult struct {
